15bfbe9e4d49c59ff967afafc006da5f366e0663
Nik Osipov <nik@safechats.com>
2015-08-27 00:47:55 +0700

    Crashlytics added

--- test-circleci-crashlytics/Pods/Target Support Files/Pods/Pods-environment.h
+++ test-circleci-crashlytics/Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,24 @@
 // project.
 
 
+// Crashlytics
+#define COCOAPODS_POD_AVAILABLE_Crashlytics
+#define COCOAPODS_VERSION_MAJOR_Crashlytics 3
+#define COCOAPODS_VERSION_MINOR_Crashlytics 1
+#define COCOAPODS_VERSION_PATCH_Crashlytics 1
+
+// Fabric
+#define COCOAPODS_POD_AVAILABLE_Fabric
+#define COCOAPODS_VERSION_MAJOR_Fabric 1
+#define COCOAPODS_VERSION_MINOR_Fabric 4
+#define COCOAPODS_VERSION_PATCH_Fabric 0
+
+// Fabric/Base
+#define COCOAPODS_POD_AVAILABLE_Fabric_Base
+#define COCOAPODS_VERSION_MAJOR_Fabric_Base 1
+#define COCOAPODS_VERSION_MINOR_Fabric_Base 4
+#define COCOAPODS_VERSION_PATCH_Fabric_Base 0
+
 // SDWebImage
 #define COCOAPODS_POD_AVAILABLE_SDWebImage
 #define COCOAPODS_VERSION_MAJOR_SDWebImage 3
