9a903972d63312043527426e396d995ebdc526c1
Nik Osipov <nik@safechats.com>
2015-08-27 00:35:33 +0700

    Project updated

--- test-circleci-crashlytics/test-circleci-crashlytics/AppDelegate.m
+++ test-circleci-crashlytics/test-circleci-crashlytics/AppDelegate.m
@@ -2,7 +2,7 @@
 //  AppDelegate.m
 //  test-circleci-crashlytics
 //
-//  Created by Nik Osipov on 26/8/15.
+//  Created by Nik Osipov on 27/8/15.
 //  Copyright (c) 2015 SafeChats Pte. Ltd. All rights reserved.
 //
 
--- test-circleci-crashlytics/test-circleci-crashlyticsTests/test_circleci_crashlyticsTests.m
+++ test-circleci-crashlytics/test-circleci-crashlyticsTests/test_circleci_crashlyticsTests.m
@@ -2,7 +2,7 @@
 //  test_circleci_crashlyticsTests.m
 //  test-circleci-crashlyticsTests
 //
-//  Created by Nik Osipov on 26/8/15.
+//  Created by Nik Osipov on 27/8/15.
 //  Copyright (c) 2015 SafeChats Pte. Ltd. All rights reserved.
 //
 
